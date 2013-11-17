3b1c6c107861f2d988b9fd4de1458fdcd8209979
alexp <alex.pirjol@gmail.com>
2013-11-17 15:20:49 +0200

    Add first template

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var mustache = require('mustache');
 var util = require('util');
 var mongodb = require('mongodb');
 var client = mongodb.MongoClient;
+var fs = require('fs');
 
 var auth = {
 	user: 'root',
@@ -10,7 +11,7 @@ var auth = {
 	host: 'paulo.mongohq.com',
 	port: 10033,
 	name: 'socialfileshare'
-}
+};
 var mongoURI = util.format('mongodb://%s:%s@%s:%d/%s',
 	auth.user, auth.pass, auth.host, auth.port, auth.name);
 
@@ -21,10 +22,19 @@ client.connect(mongoURI, {auto_reconect: true}, function(err, database){
 			console.log("Unknown error connecting to database");
 		}else{
 			console.log('Connected to MongoDB database server at:');
-	        console.log('\n\t%s\n', mongoURI);
-		}
-	}
-})
+            console.log('\n\t%s\n', mongoURI);
+            console.log(database.find);
+            app.get('/:slug', function(req, res){
+                var slug =[req.params.slug][0]; // grab the page slug
+                console.log(slug);
+                var rData = {records: {}}; // wrap the data in a global object... (mustache starts from an object then parses)
+                var page = fs.readFileSync('./templates/'+slug+'.html', "utf8"); // bring in the HTML file
+                var html = mustache.to_html(page, rData); // replace all of the data
+                res.send(html); // send to client
+            });
+        }
+    }
+});
 
 var app = express();
 
